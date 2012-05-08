d474ecec972485634f33243e0da62b246e138b0b
Rob Cameron <rwcameron@gmail.com>
2012-05-08 00:26:32 -0700

    tweaking schema

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var dgram = require("dgram");
 var Db = require("mongodb").Db;
 var Connection = require("mongodb").Connection;
 var Server = require("mongodb").Server;
-var db = new Db('logger', new Server('localhost', 27017, {}), {native_parser:true});
+var db = new Db('logger', new Server('localhost', 27017, {}));
 
 var listenPort = 1234;
 
