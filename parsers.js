f0983f87e89e1b170d08d0e1d8c614f0c87fab90
Rob Cameron <rwcameron@gmail.com>
2012-05-25 00:04:03 -0700

    modularized

---
+++ parsers.js
@@ -0,0 +1,178 @@
+var RTFlowLogObject = function(msg) {
+  this.message = msg;
+  this.log = {
+    host: '',
+    type: '',
+    subType: '',
+    datetime: ''
+  }
+  this.session = {
+    id: '',
+    elapsedTime: '',
+    ingressInt: '',
+    closeReason: ''
+  }
+  this.policy = {
+    name: ''
+  }
+  this.protocol = {
+    serviceName: '',
+    application: '',
+    nestedApplication: '',
+    encrypted: '',
+    id: ''
+  }
+  this.source = {
+    address: '',
+    port: '',
+    zone: '',
+    packets: '',
+    bytes: '',
+    nat: {
+      address: '',
+      port: '',
+      ruleName: ''
+    }
+  }
+  this.destination = {
+    address: '',
+    port: '',
+    zone: '',
+    packets: '',
+    bytes: '',
+    nat: {
+      address: '',
+      port: '',
+      ruleName: ''
+    }
+  }
+  this.user = {
+    username: '',
+    roles: ''
+  }
+};
+
+exports.RTFlowLogObject = RTFlowLogObject;
+
+RTFlowLogObject.prototype = {
+  parseLog: function() {
+    var entries = this.message.split(" ");
+    this.log.datetime = entries[1];
+    this.log.host = entries[2];
+    this.log.type = entries[3];
+    this.log.subType = entries[5];
+    var reasonArray = /reason=\"([\w\s]+)\"/.exec(message);
+    if ( !! reasonArray) {
+      this.session.closeReason = reasonArray[1];
+    } else {
+      this.session.closeReason = '';
+    };
+    for (entry in entries) {
+      var field = entries[entry].split("\"")[0];
+      var value = entries[entry].split("\"")[1];
+      if ( !! value) {
+        switch (field) {
+        case 'source-address=':
+          this.source.address = value;
+          break;
+        case 'source-port=':
+          this.source.port = value;
+          break;
+        case 'destination-address=':
+          this.destination.address = value;
+          break;
+        case 'destination-port=':
+          this.destination.port = value;
+          break;
+        case 'service-name=':
+          this.protocol.serviceName = value;
+          break;
+        case 'nat-source-address=':
+          this.source.nat.address = value;
+          break;
+        case 'nat-source-port=':
+          this.source.nat.port = value;
+          break;
+        case 'nat-destination-address=':
+          this.destination.nat.address = value;
+          break;
+        case 'nat-destination-port=':
+          this.destination.nat.port = value;
+          break;
+        case 'src-nat-rule-name=':
+          this.source.nat.ruleName = value;
+          break;
+        case 'dst-nat-rule-name=':
+          this.destination.nat.ruleName = value;
+          break;
+        case 'protocol-id=':
+          this.protocol.id = value;
+          break;
+        case 'policy-name=':
+          this.policy.name = value;
+          break;
+        case 'source-zone-name=':
+          this.source.zone = value;
+          break;
+        case 'destination-zone-name=':
+          this.destination.zone = value;
+          break;
+        case 'session-id-32=':
+          this.session.id = value;
+          break;
+        case 'packets-from-client=':
+          this.source.packets = value;
+          break;
+        case 'bytes-from-client=':
+          this.source.bytes = value;
+          break;
+        case 'packets-from-server=':
+          this.destination.packets = value;
+          break;
+        case 'bytes-from-server=':
+          this.destination.bytes = value;
+          break;
+        case 'elapsed-time=':
+          this.session.elapsedTime = value;
+          break;
+        case 'application=':
+          this.protocol.application = value;
+          break;
+        case 'nested-application=':
+          this.protocol.nestedApplication = value;
+          break;
+        case 'username=':
+          this.user.username = value;
+          break;
+        case 'roles=':
+          this.user.roles = value;
+          break;
+        case 'packet-incoming-interface=':
+          this.session.ingressInt = value;
+          break;
+        case 'encrypted=':
+          this.protocol.encrypted = value;
+          break;
+        default:
+          //console.log('')
+        }
+      };
+    };
+    this.saveToMongo();
+  },
+  saveToMongo: function() {
+    var self = this;
+    var db = new Db('logger', new Server('localhost', 27017, {}));
+    db.open(function(err, result) {
+      db.collection('logs', function(err, collection) {
+        collection.insert(self, function(err, docs) {
+          if (err) {
+            console.log(err);
+          } else {
+            db.close();
+          };
+        });
+      });
+    });
+  }
+};
--- server.js
+++ server.js
@@ -2,203 +2,37 @@ var dgram = require("dgram");
 var Db = require("mongodb").Db;
 var Connection = require("mongodb").Connection;
 var Server = require("mongodb").Server;
-var logArray = new Array();
+
+//
+var parsers = require('./parsers.js')
 
 var listenPort = 1234;
 
 var server = dgram.createSocket("udp4");
 
-server.on("message", function (msg, rinfo) {
+server.on("message", function(msg, rinfo) {
   sdSyslogParser(msg);
 });
 
-server.on("listening", function () {
+server.on("listening", function() {
   var address = server.address();
   console.log("server listening " + address.address + ":" + address.port);
 });
 
 server.bind(listenPort);
 
-
 var sdSyslogParser = function(msg) {
   var string = msg.toString('utf8');
-  var logObj = new LogObject(string);
-  var entries = string.split(" ");
-  logObj.log.datetime = entries[1];
-  logObj.log.host = entries[2];
-  logObj.log.type = entries[3];
-  logObj.log.subType = entries[5];
-  var reasonArray = /reason=\"([\w\s]+)\"/.exec(string);
-  if (!!reasonArray) {
-    logObj.session.closeReason = reasonArray[1];
-  } else {
-    logObj.session.closeReason = '';
+  var fields = string.split(" ");
+  var datetime = fields[1],
+      host = fields[2],
+      type = fields[3],
+      subType = fields[5];
+  if (type == 'RT_FLOW') {
+    var log = new parsers.RTFlowLogObject(string);
+    log.parseLog()
+  } else if (type == 'RT_IDP') {
+
   };
-  for (entry in entries) {
-    var field = entries[entry].split("\"")[0];
-    var value = entries[entry].split("\"")[1];
-    if (!!value) {
-      switch(field) {
-        case 'source-address=':
-          logObj.source.address = value;
-          break;
-        case 'source-port=':
-          logObj.source.port = value;
-          break;
-        case 'destination-address=':
-          logObj.destination.address = value;
-          break;
-        case 'destination-port=':
-          logObj.destination.port = value;
-          break;
-        case 'service-name=':
-          logObj.protocol.serviceName = value;
-          break;
-        case 'nat-source-address=':
-          logObj.source.nat.address = value;
-          break;
-        case 'nat-source-port=':
-          logObj.source.nat.port = value;
-          break;
-        case 'nat-destination-address=':
-          logObj.destination.nat.address = value;
-          break;
-        case 'nat-destination-port=':
-          logObj.destination.nat.port = value;
-          break;
-        case 'src-nat-rule-name=':
-          logObj.source.nat.ruleName = value;
-          break;
-        case 'dst-nat-rule-name=':
-          logObj.destination.nat.ruleName = value;
-          break;
-        case 'protocol-id=':
-          logObj.protocol.id = value;
-          break;
-        case 'policy-name=':
-          logObj.policy.name = value;
-          break;
-        case 'source-zone-name=':
-          logObj.source.zone = value;
-          break;
-        case 'destination-zone-name=':
-          logObj.destination.zone = value;
-          break;
-        case 'session-id-32=':
-          logObj.session.id = value;
-          break;
-        case 'packets-from-client=':
-          logObj.source.packets = value;
-          break;
-        case 'bytes-from-client=':
-          logObj.source.bytes = value;
-          break;
-        case 'packets-from-server=':
-          logObj.destination.packets = value;
-          break;
-        case 'bytes-from-server=':
-          logObj.destination.bytes = value;
-          break;
-        case 'elapsed-time=':
-          logObj.session.elapsedTime = value;
-          break;
-        case 'application=':
-          logObj.protocol.application = value;
-          break;
-        case 'nested-application=':
-          logObj.protocol.nestedApplication = value;
-          break;
-        case 'username=':
-          logObj.user.username = value;
-          break;
-        case 'roles=':
-          logObj.user.roles = value;
-          break;
-        case 'packet-incoming-interface=':
-          logObj.session.ingressInt = value;
-          break;
-        case 'encrypted=':
-          logObj.protocol.encrypted = value;
-          break;
-        default:
-          //console.log('')
-      }
-    };
-  };
-  logObj.saveToMongo();
   //return object;
-};
-
-var LogObject = function(msg) {
-  this.message = msg;
-  this.log = {
-    host: '',
-    type: '',
-    subType: '',
-    datetime: ''
-  }
-  this.session = {
-    id: '',
-    elapsedTime: '',
-    ingressInt: '',
-    closeReason: ''
-  }
-  this.policy = {
-    name: ''
-  }
-  this.protocol = {
-    serviceName: '',
-    application: '',
-    nestedApplication: '',
-    encrypted: '',
-    id:''
-  }
-  this.source = {
-    address: '',
-    port: '',
-    zone: '',
-    packets:'',
-    bytes: '',
-    nat: {
-      address: '',
-      port: '',
-      ruleName: ''
-    }
-  }
-  this.destination = {
-    address:'',
-    port: '',
-    zone: '',
-    packets: '',
-    bytes: '',
-    nat: {
-      address: '',
-      port: '',
-      ruleName: ''
-    }
-  }
-  this.user = {
-    username: '',
-    roles: ''
-  }
-};
-
-LogObject.constructor = LogObject;
-
-LogObject.prototype = {
-  saveToMongo: function() {
-    var self = this;
-    var db = new Db('logger', new Server('localhost', 27017, {}));
-    db.open(function(err,result){
-      db.collection('logs', function(err,collection){
-        collection.insert(self,function(err,docs){
-          if (err) {
-            console.log(err);
-          } else {
-            db.close();
-          };
-        });
-      });
-    });
-  }
 };
