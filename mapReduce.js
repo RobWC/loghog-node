76753920e40f79c3eb86dba3e848625ec68b12e0
Rob Cameron <rwcameron@gmail.com>
2012-05-26 11:44:52 -0700

    adding map reduce tooling

---
+++ mapReduce.js
@@ -0,0 +1,35 @@
+var Db = require('mongodb').Db,
+  Connection = require('mongodb').Connection,
+  Server = require('mongodb').Server;
+  
+var mapFunc = function() {
+  sourceAddress = this.source.address;
+  ipNumArray = sourceAddress.split('.')   
+  if (!!sourceAddress) {
+    ipnum = parseInt((16777216 * parseInt(ipNumArray[0])) + (65536 * parseInt(ipNumArray[1])) + (256 * parseInt(ipNumArray[2])) + parseInt(ipNumArray[3]));
+    if (ipnum >= 167772160 && ipnum <= 184549375) {
+     //10.x.x.x ignore
+    } else {
+     emit({'source': sourceAddress},{'count':1});
+    }
+  }
+};
+
+var reduceFunc = function(key,values) {
+  var count = 0;
+  values.forEach(function(v){
+    count += v['count'];
+  });
+  
+  return {count: count};
+};
+
+var db = new Db('logger', new Server('localhost', 27017, {}), {native_parser:true});
+db.open(function(err, db) {
+  db.collection('logs', function(err, collection) {
+    collection.mapReduce(mapFunc, reduceFunc, {out: {replace : 'no10xMR'}, verbose: true}, function(err, collection, stats) {
+      console.log(stats);
+      db.close();
+    });
+  });
+});
