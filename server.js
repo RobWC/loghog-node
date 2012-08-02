5bdec9ed823d52544ff7a414123f457b7eb1ccad
Rob Cameron <rwcameron@gmail.com>
2012-08-01 20:58:59 -0700

    emitting

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ var parser = new logParser();
 var db = new Db('logger', new Server('localhost', 27017, {}));
 var listenPort = 1234;
 
-var server = dgram.createSocket("udp4");
-
-server.on("message", function(msg, rinfo) {
-  parser.parse(msg);
-});
-
-server.on("listening", function() {
-  var address = server.address();
-  console.log("server listening " + address.address + ":" + address.port);
+db.open(function(err, result) {
+  console.log(result);
+  parser.on('save',function(data){
+    console.log(data);
+    db.collection('logs', function(err, collection) {
+      collection.insert(data);
+    });
+  });
+  
 });
 
 parser.on('test',function(data){
@@ -29,15 +29,16 @@ parser.on('save',function(data){
   console.log(data);
 });
 
-db.open(function(err, result) {
-  console.log(result);
-  parser.on('save',function(data){
-    console.log(data);
-    db.collection('logs', function(err, collection) {
-      collection.insert(data);
-    });
-  });
-  
+
+var server = dgram.createSocket("udp4");
+
+server.on("message", function(msg, rinfo) {
+  parser.parse(msg);
+});
+
+server.on("listening", function() {
+  var address = server.address();
+  console.log("server listening " + address.address + ":" + address.port);
 });
 
 server.bind(listenPort);
